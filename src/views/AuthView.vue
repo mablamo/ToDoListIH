5d94d71ba411b07731f6ddea73a5891f9941d049
Mario Blazquez <mario.blazquez.m@gmail.com>
2022-09-19 20:37:14 +0200

    creacion de logo y auth view

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,25 +1,40 @@
 <template>
-  <h1>Auth view</h1>
-  <form>
-    <label for="email"><input type="email" id="email" v-model="email">email</label>
-    <label for="password"><input type="password" id="password" v-model="password">password</label>
-    <label for="condirmPassword">
-      <input type="password" id="confirmPassword" v-model="confirmPassword">confirm password
-    </label>
-    <button @click="handleSignUp">Sign Up</button>
-  </form>
-  <form>
-    <label for="email"><input type="email" id="signInEmail" v-model="signInEmail">email</label>
-    <label for="password"><input type="password" id="signInPassword" v-model="signInPassword">
-    password
-    </label>
-    <button @click="handleSignIn" type="button">Sign In</button>
-  </form>
+  <div class="position-absolute top-50 start-50 translate-middle auth">
+    <h1>Welcome to</h1>
+    <img src='../assets/logo.png' alt="Logo_Dori">
+    <h2>don't forget anything!</h2>
+
+    <div
+      class="btn-group"
+      role="group"
+      aria-label="Basic radio toggle button group"
+    >
+      <label class="btn btn-outline-primary" for="btnradio1">
+        I have an account
+        <input
+          type="radio"
+          class="btn-check"
+          name="btnradio"
+          id="btnradio1"
+          checked
+          @click="logedUser"
+      /></label>
+      <label class="btn btn-outline-primary" for="btnradio2"
+        >Create new account
+        <input type="radio" class="btn-check" name="btnradio" id="btnradio2" @click="unlogedUser"/>
+      </label>
+    </div>
+
+    <sign-in-component class="auth-component" v-show="loged"></sign-in-component>
+    <sign-up-component class="auth-component" v-show="!loged"></sign-up-component>
+  </div>
 </template>
 
 <script>
 import { mapState, mapActions } from 'pinia';
 import userStore from '@/store/user';
+import SignInComponent from '../components/SignInComponent.vue';
+import SignUpComponent from '../components/SignUpComponent.vue';
 
 export default {
   name: 'AuthView',
@@ -30,7 +45,12 @@ export default {
       confirmPassword: '',
       signInEmail: '',
       signInPassword: '',
+      loged: true,
     };
+  },
+  components: {
+    SignInComponent,
+    SignUpComponent,
   },
   computed: {
     ...mapState(userStore, ['user']),
@@ -44,8 +64,11 @@ export default {
         console.log('password no coincide');
       }
     },
-    handleSignIn() {
-      this.signIn(this.signInEmail, this.signInPassword);
+    unlogedUser() {
+      this.loged = false;
+    },
+    logedUser() {
+      this.loged = true;
     },
   },
   watch: {
@@ -57,3 +80,24 @@ export default {
   },
 };
 </script>
+
+<style>
+img {
+  width: 50%;
+}
+
+.auth {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.auth-component {
+  margin: 15px 0;
+}
+
+h2 {
+  margin-bottom: 50px;
+}
+
+</style>
